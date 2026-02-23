74bfcd7fb16c5bc0b0365c48392b88366cf6b724
gonzalo <larroyagonzalo+git@gmail.com>
2026-02-23 15:23:10 +0000

    feat: `ASaveLoader` code implementation

--- src/main/java/CoffeeShop/SaveLoader.java
+++
@@ -1,4 +0,0 @@
-package CoffeeShop;
-
-public class SaveLoader {
-}
--- src/main/java/CoffeeShop/SaveLoader/SaveLoaderOrders.java
+++ src/main/java/CoffeeShop/SaveLoader/SaveLoaderOrders.java
@@ -1,20 +1,47 @@
 package CoffeeShop.SaveLoader;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import CoffeeShop.Customer;
 import CoffeeShop.Order;
 
 public class SaveLoaderOrders extends ASaveLoader<Order> {
 
-	SaveLoaderOrders(String readPath, String writePath) throws IOException {
+	List<Customer> _customers;
+
+	SaveLoaderOrders(String readPath, String writePath, List<Customer> customers) throws IOException {
 		super(readPath, writePath);
+
+		this._customers = customers;
+	}
+
+	private Order ParseLine(String line) {
+		String[] values = line.split(",");
+
+		for (String value : values ) {
+
+		}
+
+		return null;
 	}
 
 	@Override
-	public List<Order> LoadData(String path) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'LoadData'");
+	public List<Order> LoadData(String path) throws LoadingException  {
+		List<Order> orders = new ArrayList<Order>();
+		List<String> lines = this.ReadFile();
+
+		for (String line : lines) {
+			Order order = this.ParseLine(line);
+
+			if (order != null) {
+				orders.add(order);
+			}
+
+		}
+
+		return orders;
 	}
 
 	@Override
